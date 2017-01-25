7c63ae323478c40f8091cc8fd19cd23c3883402b
skiptilykill <gudmundurjo295@nemi.tskoli.is>
2017-01-25 10:56:33 +0000

    Dæmi 1

---
+++ Gitverkefni.py
@@ -0,0 +1,10 @@
+#Guðmundur jóns
+#25.1.2017
+
+#Dæmi 1
+
+tala1 = int(input("Sláðu inn tölu: "))
+tala2 = int(input("sláðu inn aðra tölu: "))
+
+print("Tölurnar lagðar saman: ", tala1+tala2)
+print("Tölurnar margfaldaðar: ", tala1*tala2)
